46e40b2d1644062649b33803da74f460a24f3c7c
Geeoon <geeoon13@gmail.com>
2022-11-07 19:35:17 -0600

    parsing JSON from server

--- client/script.js
+++ client/script.js
@@ -5,7 +5,8 @@ const socket_url = "ws://localhost:3001/videostream";
 function connect() {
     socket = new WebSocket(socket_url);
     socket.addEventListener('message', (event) => {
-        console.log(event.data);
+        let data = JSON.parse(event.data);
+        console.log(data.data);
         /*
         jmuxer.feed({
             video: new Uint8Array(atob(event.data))  // decode from base64
